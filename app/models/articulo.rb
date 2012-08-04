9d9684a9a2d8b018874fc1ec8b6392a32356e164
holberg <angel.holberg@gmail.com>
2012-08-04 13:07:07 -0500

    Modifique dos modelos articulo y medida

--- app/models/articulo.rb
+++ app/models/articulo.rb
@@ -1,6 +1,6 @@
 class Articulo < ActiveRecord::Base
   validates :clave, :presence => true, :length => {:maximum => 20, :minimum => 3}, :uniqueness => { :message => "La clave ya existe!"  }
-  has_many :articulos_medidas
-  has_many :medidas, :through => :relaciones_articulos_medidas
+  has_many :relaciones_articulos_medida
+  has_many :medidas, :through => :relaciones_articulos_medida
   attr_accessible :clave, :descripcion, :descripcion_larga
 end
