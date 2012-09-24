a20a384b417b638fb8ab308c6177839b3ee7eda9
holberg <angel.holberg@gmail.com>
2012-09-23 19:34:44 -0500

    modifique mi pdf de osy la imagen de los activos

--- app/pdfs/orden_servicio_pdf.rb
+++ app/pdfs/orden_servicio_pdf.rb
@@ -21,17 +21,21 @@ class OrdenServicioPdf < Prawn::Document
     if @orden_servicio.activo.codigo?
       doc=RGhost::Document.new
       doc.barcode_code39("#{@orden_servicio.activo.codigo}",:columns => 2, :rows=> 2, :text => {:size => 10})
-      doc.render :jpeg, :filename => "#{Rails.root}/tmp/barcode.jpeg"                          
-      image "#{Rails.root}/tmp/barcode.jpeg" , :at => [100,580], :width => 500
+      doc.render :jpeg, :filename => "#{Rails.root}/tmp/barcode.jpeg"
+      if File.exist?("#{Rails.root}/tmp/barcode.jpeg")                          
+        image "#{Rails.root}/tmp/barcode.jpeg" , :at => [100,580], :width => 500
+      else
+        text "No se pudo generar el código de barras, formato soportado = code39", size: 6
+      end
     else
-      text "Sin código de barras"
+      text "Sin código de barras", size: 6
     end
     text "Imagen:", size: 11, style: :bold
     if @orden_servicio.activo.imagen_url?
       if File.exist?("#{@orden_servicio.activo.imagen_url}")
         image "#{@orden_servicio.activo.imagen_url}" , :at => [300,580], :width => 160
       else
-        text "No existe la imagen"
+        text "No existe la imagen", size: 6
       end
     end                                      
     
