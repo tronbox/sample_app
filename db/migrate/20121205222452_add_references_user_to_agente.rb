aa1afa2cf6c2a3a76b2cc7419657870f6221c7d0
angelholberg <ahracnmr@gmail.com>
2012-12-05 16:38:14 -0600

    Agregue la realacion del usuario con el agente

---
+++ db/migrate/20121205222452_add_references_user_to_agente.rb
@@ -0,0 +1,13 @@
+class AddReferencesUserToAgente < ActiveRecord::Migration
+  def up
+    change_table :agentes do |t|
+      t.references :user
+    end
+    add_index :agentes, :user_id
+  end
+
+  def down
+    remove_column :agentos, :user
+    remove_index :agentes, :user_id
+  end
+end
