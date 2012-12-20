bcbb17025e7f5830f5549b59e5e5fc226c59afe6
angelholberg <ahracnmr@gmail.com>
2012-12-19 21:15:28 -0600

    Modifique la relacion en usuario-agente y el before_create en agente

--- app/models/agente.rb
+++ app/models/agente.rb
@@ -1,11 +1,26 @@
 class Agente < ActiveRecord::Base
   has_many :recepcion_activo
   belongs_to :nave
-  belongs_to :user
-  validate :valida_responsable_for_nave
+  belongs_to :user 
+  #validate :valida_responsable_for_nave
   validates :clave, :presence => true, :length => {:maximum => 20, :minimum => 3}, :uniqueness => { :message => "La clave ya existe!"  }
   attr_accessible :clave, :nombre, :e_mail, :responsable, :nave_id,  :user_id
   
+  before_create do |agente|   
+
+    #if Agente.find_by_nave_id_and_responsable(5, TRUE)      
+    # errors.add(:nave_id, "Ya existe en la BD")
+    #end
+    #puts "**********************************************************************************"
+    #puts self.nave_id
+    #puts Agente.find_by_nave_id_and_responsable(self.nave_id, TRUE)
+    if agente.responsable?  
+      errors.add(:nave_id, "Ya existe un Responsable para esta Nave Industrial") if Agente.find_by_nave_id_and_responsable(agente.nave_id, TRUE)
+      return false
+    end
+  end
+  
+    
   before_destroy :valida_dependencias
   
   protected
@@ -17,15 +32,5 @@ class Agente < ActiveRecord::Base
     end            
   end
   
-  def valida_responsable_for_nave
-    #if Agente.find_by_nave_id_and_responsable(5, TRUE)      
-    # errors.add(:nave_id, "Ya existe en la BD")
-    #end
-    #puts "**********************************************************************************"
-    #puts self.nave_id
-    #puts Agente.find_by_nave_id_and_responsable(self.nave_id, TRUE)
-    if self.responsable?
-      errors.add(:nave_id, "Ya existe un Responsable para esta Nave Industrial") if Agente.find_by_nave_id_and_responsable(self.nave_id, TRUE)
-    end
-  end
+  
 end
